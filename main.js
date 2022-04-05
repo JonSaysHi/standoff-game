ed15a730d1708a50164da04fa3b76cf23e03904f
Jon Hii <jonathan.hii@outlook.com>
2022-04-06 03:31:04 +1000

    addEventListener for shoot added, randomized computer play added

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-const shoot = document.querySelector("#shootBtn");
-const reload = document.querySelector("#reloadBtn");
-const block = document.querySelector("#blockBtn");
+let playerBullets = 0;
+let computerBullets = 0;
+const shoot = document.querySelector("#shoot");
+const reload = document.querySelector("#reload");
+const block = document.querySelector("#block");
+
+shoot.addEventListener("click", function () {
+  console.log("shoot");
+});
+
+function computerPlay() {
+  let options = ["shoot", "reload", "block"];
+  return options[Math.floor(Math.random() * options.length)];
+}
