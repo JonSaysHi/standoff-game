1e15e9dfbdbd17d949175dd7316e3d898826aa08
Jon Hii <jonathan.hii@outlook.com>
2022-04-07 01:56:16 +1000

    recovery of sounds and images

--- main.js
+++ main.js
@@ -5,74 +5,94 @@ const reload = document.querySelector("#reload");
 const block = document.querySelector("#block");
 
 shoot.addEventListener("click", () => {
-  let playerSelection = "shoot";
-  let computerSelection = computerPlay();
-  const result = playRound(playerSelection, computerSelection);
-  console.log("Player Selection: " + playerSelection);
-  console.log("Computer Selection: " + computerSelection);
-  console.log(result);
-  console.log(playerBullets);
-  console.log(computerBullets);
+  if (playerBullets > 0) {
+    let playerSelection = "Shoot";
+    let computerSelection = computerPlay();
+    const result = playRound(playerSelection, computerSelection);
+    document.querySelector("#p-move").textContent = playerSelection;
+    document.querySelector("#c-move").textContent = computerSelection;
+    document.querySelector("#result").textContent = result;
+    document.querySelector("#p-score").textContent = playerBullets;
+    document.querySelector("#c-score").textContent = computerBullets;
+    shootSound();
+  } else if (playerBullets === 0) {
+    let playerSelection = "Empty";
+    let computerSelection = " - ";
+    let result = "You have no bullets. You must reload.";
+    document.querySelector("#p-move").textContent = playerSelection;
+    document.querySelector("#c-move").textContent = computerSelection;
+    document.querySelector("#result").textContent = result;
+    document.querySelector("#p-score").textContent = playerBullets;
+    document.querySelector("#c-score").textContent = computerBullets;
+    emptySound();
+  }
 });
 
 reload.addEventListener("click", () => {
-  let playerSelection = "reload";
+  let playerSelection = "Reload";
   let computerSelection = computerPlay();
   const result = playRound(playerSelection, computerSelection);
-  console.log("Player Selection: " + playerSelection);
-  console.log("Computer Selection: " + computerSelection);
-  console.log(result);
-  console.log(playerBullets);
-  console.log(computerBullets);
+  document.querySelector("#p-move").textContent = playerSelection;
+  document.querySelector("#c-move").textContent = computerSelection;
+  document.querySelector("#result").textContent = result;
+  document.querySelector("#p-score").textContent = playerBullets;
+  document.querySelector("#c-score").textContent = computerBullets;
+  reloadSound();
 });
 
 block.addEventListener("click", () => {
-  let playerSelection = "block";
+  let playerSelection = "Block";
   let computerSelection = computerPlay();
   const result = playRound(playerSelection, computerSelection);
-  console.log("Player Selection: " + playerSelection);
-  console.log("Computer Selection: " + computerSelection);
-  console.log(result);
-  console.log(playerBullets);
-  console.log(computerBullets);
+  document.querySelector("#p-move").textContent = playerSelection;
+  document.querySelector("#c-move").textContent = computerSelection;
+  document.querySelector("#result").textContent = result;
+  document.querySelector("#p-score").textContent = playerBullets;
+  document.querySelector("#c-score").textContent = computerBullets;
+  blockSound();
 });
 
 function playRound(playerSelection, computerSelection) {
   let result;
-  if (playerBullets === 6 && playerSelection === "reload") {
+  if (playerBullets === 6 && playerSelection === "Reload") {
     result = "Your gun is full, you must shoot or block.";
-  } else if (playerBullets <= 0 && playerSelection === "shoot") {
-    result = "You have no bullets. You must reload";
-  } else if (playerSelection === "block" && computerSelection === "block") {
+    //   } else if (playerBullets <= 0 && playerSelection === "Shoot") {
+    //     result = "You have no bullets. You must reload.";
+  } else if (playerSelection === "Block" && computerSelection === "Block") {
     result = "";
-  } else if (playerSelection === "reload" && computerSelection === "reload") {
+  } else if (playerSelection === "Reload" && computerSelection === "Reload") {
     result = "";
     computerBullets++;
     playerBullets++;
-  } else if (playerSelection === "reload" && computerSelection === "block") {
+  } else if (playerSelection === "Reload" && computerSelection === "Block") {
     result = "";
     playerBullets++;
-  } else if (playerSelection === "block" && computerBullets === 0) {
-    result =
-      "You have no bullets, and neither have they. You should probably reload.";
-  } else if (playerSelection === "shoot" && computerSelection === "shoot") {
-    result = "Your shots cancel each other out";
+  } else if (playerSelection === "Block" && computerBullets === 0) {
+    result = "You should reload. Neither of you have bullets.";
+  } else if (playerSelection === "Shoot" && computerSelection === "Shoot") {
+    result = "Your shots cancel each other out.";
     playerBullets--;
     computerBullets--;
-  } else if (playerSelection === "shoot" && computerSelection === "block") {
+    ricochetSound();
+  } else if (playerSelection === "Shoot" && computerSelection === "Block") {
     result = "They blocked your shot!";
     playerBullets--;
-  } else if (playerSelection === "shoot" && computerSelection === "reload") {
+    ricochetSound();
+  } else if (playerSelection === "Shoot" && computerSelection === "Reload") {
     result = "You win!";
     playerBullets = 0;
     computerBullets = 0;
-  } else if (playerSelection === "block" && computerSelection === "shoot") {
+    setTimeout(victorySound, 1000);
+  } else if (playerSelection === "Block" && computerSelection === "Shoot") {
     result = "You blocked their shot!";
+    ricochetSound();
     computerBullets--;
-  } else if (playerSelection === "reload" && computerSelection === "shoot") {
+  } else if (playerSelection === "Reload" && computerSelection === "Shoot") {
     result = "You lose!";
     playerBullets = 0;
     computerBullets = 0;
+    shootSound();
+    setTimeout(lossSound, 1000);
   } else {
     return;
   }
@@ -81,39 +101,53 @@ function playRound(playerSelection, computerSelection) {
 
 function computerPlay() {
   if (computerBullets === 6) {
-    let options = ["shoot", "block"];
+    let options = ["Shoot", "Block"];
     return options[Math.floor(Math.random() * options.length)];
   } else if (computerBullets === 0 && playerBullets === 0) {
-    return "reload";
+    return "Reload";
   } else if (computerBullets > 0 && playerBullets === 0) {
-    let options = ["shoot", "reload"];
+    let options = ["Shoot", "Reload"];
     return options[Math.floor(Math.random() * options.length)];
   } else if (computerBullets === 0 && playerBullets > 0) {
-    let options = ["reload", "block"];
+    let options = ["Reload", "Block"];
     return options[Math.floor(Math.random() * options.length)];
   } else if (computerBullets >= 1 && playerBullets >= 1) {
-    let options = ["shoot", "reload", "block"];
+    let options = ["Shoot", "Reload", "Block"];
     return options[Math.floor(Math.random() * options.length)];
   }
 }
 
-function checkWinner() {
-  if (result === "You win!") {
-    winner =
-      "Congratulations, you won. Choose your next move to restart the game!";
-    playerScore = 0;
-    computerScore = 0;
-  } else if (result === "You lose!") {
-    winner = "Unlucky, you lost. Choose your next move to restart the game!";
-    playerScore = 0;
-    computerScore = 0;
-  }
-  document.querySelector("#checkWinner").innerHTML = winner;
+function shootSound() {
+  let audio = new Audio("sounds/Shoot.mp3");
+  audio.play();
 }
 
-function reset() {
-  if (playerScore === 0 || computerScore === 0) {
-    winner = "";
-  }
-  document.querySelector("#checkWinner").innerHTML = winner;
+function reloadSound() {
+  let audio = new Audio("sounds/Reload.wav");
+  audio.play();
+}
+
+function ricochetSound() {
+  let audio = new Audio("sounds/Ricochet.wav");
+  audio.play();
+}
+
+function emptySound() {
+  let audio = new Audio("sounds/Empty.wav");
+  audio.play();
+}
+
+function blockSound() {
+  let audio = new Audio("sounds/Block.mp3");
+  audio.play();
+}
+
+function victorySound() {
+  let audio = new Audio("sounds/Victory.mp3");
+  audio.play();
+}
+
+function lossSound() {
+  let audio = new Audio("sounds/Loss.mp3");
+  audio.play();
 }
